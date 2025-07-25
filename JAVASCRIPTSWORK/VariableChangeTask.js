5c9c00fbc807db74a2f8dd9c3ea5bac71e304ec6
EngrEOOnoja <onojaemmanuel193@gmail.com>
2025-07-25 23:16:03 +0100

    Task Progress

---
+++ JAVASCRIPTSWORK/VariableChangeTask.js
@@ -0,0 +1,14 @@
+let var1 = 2;
+let var2 = 3;
+let var3 = 4;
+var1 = var1 + var2 + var3;
+var2 = var1 - var2 - var3;
+var3 = var1 - var2 - var3;
+var1 = var1 - var2 - var3;
+
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
+
+
---
+++ JAVASCRIPTSWORK/investmentreturn.py
@@ -0,0 +1,5 @@
+var number = 1;
+console.log(number);
+
+var number = 2;
+console.log(number);
