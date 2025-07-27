68da95a9aee09071b78ed4ef415c984dbe1f33d3
EngrEOOnoja <onojaemmanuel193@gmail.com>
2025-07-27 17:30:25 +0100

    GradeCalculatorProgress

---
+++ JAVASCRIPTSWORK/TaskGradeCalculator.js
@@ -0,0 +1,60 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout
+});
+function getScore() {
+  rl.question('Please enter a value between 0 and 100: ', score => {
+  score = parseInt(score);
+  if (score >= 0 && score <= 100) {
+  let grade = calculateGrade(score);
+  let feedback = getFeedback(grade);
+  console.log(`Grade: ${grade}`);
+  console.log(`Feedback: ${feedback}`);
+  askAgain();
+  } else {
+   console.log('Invalid input. Please try again.');
+   getScore();
+    }
+  });
+}
+
+function calculateGrade(score) {
+ if (score >= 90) {
+  return 'A';
+ } else if (score >= 80) {
+ return 'B';
+  } else if (score >= 70) {
+ return 'C';
+  } else if (score >= 60) {
+  return 'D';
+ } else {
+ return 'F';
+  }
+}
+
+function getFeedback(grade) {
+if (grade === 'A') {
+ return 'Excellent performance';
+  } else if (grade === 'B') {
+  return 'Good job, Keep improving';
+  } else if (grade === 'C') {
+  return 'Fair effort, keep working on it';
+  } else if (grade === 'D') {
+  return 'You can do better, don’t give up';
+  } else {
+ return 'Needs Improvement, Ask for help';
+  }
+}
+
+function askAgain() {
+rl.question('Try another score? (yes/no): ', answer => {
+if (answer.toLowerCase() === 'yes') {
+getScore();
+} else {
+rl.close();
+    }
+  });
+}
+
+getScore();
--- JAVASCRIPTSWORK/investmentreturn.py
+++
@@ -1,5 +0,0 @@
-var number = 1;
-console.log(number);
-
-var number = 2;
-console.log(number);
