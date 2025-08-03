62eb10ff10b99f77791b6e42307bd4fe6cec84ec
EngrEOOnoja <onojaemmanuel193@gmail.com>
2025-08-03 08:36:25 +0100

    Assignment and Class task

---
+++ JavaScripts/triangle.js
@@ -0,0 +1,23 @@
+
+	triangle = 0;
+	
+for(index = 0; index < 6; index++){
+	let line = "";	
+	for(angle = 0; angle < index; angle++){
+	triangle += 1;		
+		if (triangle < 10 ){
+		line += "  ncg/.[p
+		
+		;	
+	}
+	
+		
+	line = line + " " + triangle ; 
+		
+		
+			}
+			
+			console.log(line)
+
+		}
+		
